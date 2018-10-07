2e0eddc5d8f14e16ade2e77ca76af35f21a2d98b
Juan Ledesma <a01206568@itesm.mx>
2018-10-07 13:22:26 -0500

    Initial documents

---
+++ parse.rb
@@ -0,0 +1,3 @@
+print "Something to digest: " #Prints without a new line
+info = gets.chomp #To obtain information from the command line
+puts "What you entered was #{info}" #Adds a new line (enter) to the text
