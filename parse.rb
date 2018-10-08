30c380ebf713bad64b9a284db298e79806228dac
LeonRodriguezG <a01700687@itesm.mx>
2018-10-08 16:40:55 -0500

    1st Parse

--- parse.rb
+++ parse.rb
@@ -1,3 +1,20 @@
 print "Something to digest: " #Prints without a new line
-info = gets.chomp #To obtain information from the command line
-puts "What you entered was #{info}" #Adds a new line (enter) to the text
+
+#info = gets.chomp #To obtain information from the command line
+Var_names = gets.chomp.gsub(/ /,'').split(',')
+Var_names.each {|i| puts i}
+
+num = gets.chomp
+probs=[]
+num.to_i.times do
+  probs.push gets.chomp
+end
+
+num = gets.chomp
+query=[]
+num.to_i.times do
+  query.push gets.chomp
+end
+
+
+#puts "What you entered was #{info}" #Adds a new line (enter) to the text
